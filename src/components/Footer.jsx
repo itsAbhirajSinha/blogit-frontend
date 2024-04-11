7f0bfa4db00cbff3f178cabed6bb53e52d2fcc0a
godlovesus <abhirajsinha777@gmail.com>
2024-04-11 12:49:15 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+  return (
+    <>
+<div className="mt-8 w-full bg-black px-8 md:px-[300px]  space-y-6 md:space-y-0 items-start md:justify-between text-sm md:text-md py-8">
+    <p className="py-2 pb-6 text-center text-white bg-black text-sm"> @BlogIT 2024 All Rights Reserved</p>
+      
+    </div>
+    </>
+    
+  )
+}
+
+export default Footer
